9b3f8c28e381e5dce31931c1478a139f29ac065f
Emmanuel-Begati <e.yakubu@alustudent.com>
2023-07-06 15:11:40 +0200

    did what i can do#

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+response = requests.get('https://jsonplaceholder.typicode.com/users')
+todo_list = requests.get('https://jsonplaceholder.typicode.com/todos')
+
+def task_title(name):
+    userId = name + 1
+    counter = []
+    for i in todo_list.json():
+        if i['userId'] == userId:
+            if i['completed'] == True:
+                counter.append('True')
+            
+    employee_name = response.json()[name]['name']
+    number_of_done_tasks = len(counter)
+    print (employee_name, 'has completed',number_of_done_tasks , '/20 tasks')
+
+name = 0
+while name < 10:
+    print (task_title(name))
+    name += 1
+    
