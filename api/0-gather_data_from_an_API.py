9d403548bff2f88c5a6189aeef8e9bed47f75139
Emmanuel-Begati <e.yakubu@alustudent.com>
2023-07-06 15:55:30 +0200

    done, i guess

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -18,10 +18,13 @@ def task_title(name):
     print (employee_name, 'has completed',number_of_done_tasks , '/20 tasks')
     for task in todo_list.json():
         if task['completed'] == True:
-            print (task['title'])
+            if task['userId'] == userId:
+                print (task['title'])
+    
 
 name = 0
 while name < 10:
-    print (task_title(name))
+    (task_title(name))
     name += 1
     
+
