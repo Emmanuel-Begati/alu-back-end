7da8a5b1d415ec3e640426bd1f67d8152cbe81e3
Emmanuel-Begati <e.yakubu@alustudent.com>
2023-07-17 01:39:18 +0200

    corrections made

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -25,15 +25,15 @@ if __name__ == "__main__":
     userId = name
     str_userId = str(userId)
     user = response.json()[(name - 1)]["username"]
+    for task in todo_list.json():
+            the_task = []
+            if task['userId'] == userId:
+                done = task['completed']
+                # print(userId, user, done, task['title'])
+                u_task = task['title']
+                the_task.append({"task": u_task, 'completed': done, 'username': user})
     with open(f"{userId}.json", "w") as file:
-        for task in todo_list.json():
-                the_task = []
-                if task['userId'] == userId:
-                    done = task['completed']
-                    # print(userId, user, done, task['title'])
-                    u_task = task['title']
-                    the_task.append({"task": u_task, 'completed': done, 'username': user})
-                    data = {str_userId: the_task}
-                    json_data = json.dumps(data)
-                    file.write(json_data)
+        data = {str_userId: the_task}
+        json_data = json.dumps(data)
+        file.write(json_data)
     (task_title(name))
